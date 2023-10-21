47f78a1b18db322e03cf3f2547159f9e588763eb
Fahmi Syaifudin <fahmi@ekan.id>
2023-10-21 13:41:16 +0700

    Fix: deploy to app engine debug

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello world" });
+  res.json({ message: "Hactoberfest Surabaya 2023" });
 });
 
 module.exports = app;
